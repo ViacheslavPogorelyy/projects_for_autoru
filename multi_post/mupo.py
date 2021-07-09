7aa3da4785463f704c981f50bb282cdd7de82cbb
ViacheslavPogorelyy <65274737+ViacheslavPogorelyy@users.noreply.github.com>
2021-07-09 17:43:47 +0300

    Add files via upload

--- multi_post/mupo.py
+++ multi_post/mupo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[50]:
 
 
 import os, os.path
@@ -9,15 +9,18 @@ import glob
 import shutil
 import csv
 import datetime
+import pandas as pd 
+import numpy as np
+import xml.etree.ElementTree as ET
 
 
-# In[7]:
+# In[51]:
 
 
 now = datetime.datetime.now()
 
 
-# In[9]:
+# In[52]:
 
 
 day = str(now.day)
@@ -29,109 +32,60 @@ second = str(now.second)
 microsecond = str(now.microsecond)
 
 
-# In[17]:
+# In[53]:
 
 
 csv_n = 'https://docs.google.com/spreadsheets/d/1FLgQtch4aZWjdVnRYh_ts9DReIS0gp2LvUyzt7E8XXk/export?format=csv'
 
 
-# In[10]:
+# In[54]:
 
 
-files = glob.glob("raw/*.csv")
-file_count = len(files)
-dir = "raw/"
+df = pd.read_csv(csv_n)
 
 
-# In[11]:
+# In[55]:
 
 
-def ConvertToXML():
-    for i in range(len(files)):
-
-        # Take input.csv and set the file output to have a unique name
-        csvFile = files[i]
-        xmlFile = 'complete/13088' + day + month + year + hour + minute + second + str(i) + '.xml'
-
-        # Remove directorry
-        file = csvFile.strip(dir)
-
-        # Open the csv file to read
-        csvData = csv.reader(open(csvFile))
-
-        # Open xml file created earlier and write a header
-        xmlData = open(xmlFile, 'w')
-        xmlData.write('<?xml version="1.0" encoding="ISO-8859-1" ?>' + "\n")
-        xmlData.write('<Orders>' + "\n")
-        xmlData.write('<Order>' + "\n")
-        xmlData.write('<Order_Date><![CDATA[' + day + '/' + month +'/' + year + ']]></Order_Date>' + "\n")
-
-        # Add your order ID format - example. ID-00000
-        xmlData.write('<Order_ID><![CDATA[DT-13088' + day + month + year + hour + minute + second + str(i) + ']]></Order_ID>' + "\n")
-
-        # Add your customer ID - example. ID000
-        xmlData.write('<Customer_ID><![CDATA[DEPN000]]></Customer_ID>' + "\n")
-        xmlData.write('<Products>' + "\n")
-
-        # Parse the csv file and write the styled information to an xml file
-        rowNum = 0
-
-        for row in csvData:
-            if rowNum == 0:
-                tags = row
-                for i in range(len(tags)):
-                    tags[i] = tags[i].replace(' ', '_')
-                    # Fix SKU
-                    if tags[i] == '_SKU':
-                        tags[i] = tags[i].replace('_SKU', 'SKU')
-                    else:
-                        tags[i] == tags[i]
-                    # Fix Quantity
-                    if tags[i] == '_Quantity':
-                        tags[i] = tags[i].replace('_Quantity', 'Quantity')
-                    else:
-                        tags[i] == tags[i]
-
-                    # Fix Item Description
-                    if tags[i] == '_Item_Description':
-                        tags[i] = tags[i].replace('_Item_Description', 'Item_Description')
-                    else:
-                        tags[i] == tags[i]
-
-                    # Fix Product Variation Details
-                    if tags[i] == '_Product_Variation_Details':
-                        tags[i] = tags[i].replace('_Product_Variation_Details', 'Product_Variation_Details')
-                    else:
-                        tags[i] == tags[i]
-
-            else:
-                xmlData.write('<Item>' + "\n")
-                for i in range(len(tags)):
-
-                    if len(row) < 5:
-                        while len(row) < 5:
-                            row.append("null")
-
-                    # Write to xml file
-                    xmlData.write('<' + tags[i] + '>'                                       + '<![CDATA[' + row[i] + ']]>' + '</' + tags[i] + '>' + "\n")
-
-                xmlData.write('</Item>' + "\n")
-
-            rowNum +=1
-
-        xmlData.write('</Products>' + "\n")
-        xmlData.write('</Order>' + "\n")
-        xmlData.write('</Orders>' + "\n")
-
-        xmlData.close()
-        MoveFile(file)
-
-def MoveFile( FileToMove ):
-    os.remove("raw/" + FileToMove)
+df 
 
 
-# In[12]:
+# In[56]:
 
 
-ConvertToXML()
+#auto_ru = '<?xml version="1.0" encoding="utf-8"?>'
+
+
+# In[57]:
+
+
+parts = ET.Element('parts')
+part = ET.SubElement(parts, 'part')
+id_part = ET.SubElement(part, 'id')
+title = ET.SubElement(part, 'title')
+stores = ET.SubElement(part, 'stores')
+store = ET.SubElement(stores, 'store')
+part_number = ET.SubElement(part, 'part_number')
+offer_url = ET.SubElement(part, 'offer_url')
+manufacturer = ET.SubElement(part, 'manufacturer')
+description = ET.SubElement(part, 'description')
+is_new = ET.SubElement(part, 'is_new')
+price = ET.SubElement(part, 'price')
+availability = ET.SubElement(part, 'availability')
+isAvailable = ET.SubElement(availability, 'isAvailable')
+images = ET.SubElement(part, 'images')
+image = ET.SubElement(images, 'image')
+
+
+# In[58]:
+
+
+tree = ET.ElementTree(parts)
+tree.write("sample.xml")
+
+
+# In[ ]:
+
+
+
 
