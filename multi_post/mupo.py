a8406d5a6ce7f1ae8b819235683f851cc701dcaa
ViacheslavPogorelyy <65274737+ViacheslavPogorelyy@users.noreply.github.com>
2021-07-15 18:17:21 +0300

    Add files via upload

--- multi_post/mupo.py
+++ multi_post/mupo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[29]:
+# In[51]:
 
 
 import os, os.path
@@ -12,15 +12,16 @@ import datetime
 import pandas as pd 
 import numpy as np
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 
-# In[30]:
+# In[52]:
 
 
 now = datetime.datetime.now()
 
 
-# In[31]:
+# In[53]:
 
 
 day = str(now.day)
@@ -32,80 +33,58 @@ second = str(now.second)
 microsecond = str(now.microsecond)
 
 
-# In[32]:
+# In[54]:
 
 
 csv_n = 'https://docs.google.com/spreadsheets/d/1FLgQtch4aZWjdVnRYh_ts9DReIS0gp2LvUyzt7E8XXk/export?format=csv'
 
 
-# In[33]:
+# In[55]:
 
 
 df = pd.read_csv(csv_n)
 
 
-# In[35]:
+# In[58]:
 
 
 parts = ET.Element('parts')
-part = ET.SubElement(parts, 'part')
-id_part = ET.SubElement(part, 'id')
-title = ET.SubElement(part, 'title')
-stores = ET.SubElement(part, 'stores')
-store = ET.SubElement(stores, 'store')
-part_number = ET.SubElement(part, 'part_number')
-offer_url = ET.SubElement(part, 'offer_url')
-manufacturer = ET.SubElement(part, 'manufacturer')
-description = ET.SubElement(part, 'description')
-is_new = ET.SubElement(part, 'is_new')
-price = ET.SubElement(part, 'price')
-availability = ET.SubElement(part, 'availability')
-isAvailable = ET.SubElement(availability, 'isAvailable')
-images = ET.SubElement(part, 'images')
-image = ET.SubElement(images, 'image')
-compatibility = ET.SubElement(part, 'compatibility')
-car = ET.SubElement(compatibility, 'compatibility')
-
-
-# In[43]:
-
-
 for i in range(len(df['id'])):
+    part = ET.SubElement(parts, 'part')
+    id_part = ET.SubElement(part, 'id')
+    title = ET.SubElement(part, 'title')
     title.text = df['title'][i]
-    description.text = df['description'][i]
-    price.text = df['price'][i]
-    isAvailable.text = 'True'
-    image.text = df['images'][i]
-    is_new = df['is_new'][i]
-    car.text = df['compatibility'][i]
+    stores = ET.SubElement(part, 'stores')
+    store = ET.SubElement(stores, 'store')
+    part_number = ET.SubElement(part, 'part_number')
     part_number.text = df['part_number'][i]
-    manufacturer.text = df['manufacturer'][i]
+    offer_url = ET.SubElement(part, 'offer_url')
     offer_url.text = df['url'][i]
+    manufacturer = ET.SubElement(part, 'manufacturer')
+    manufacturer.text = df['manufacturer'][i]
+    description = ET.SubElement(part, 'description')
+    description.text = df['description'][i]
+    is_new = ET.SubElement(part, 'is_new')
+    is_new.text = df['is_new'][i]
+    price = ET.SubElement(part, 'price')
+    price.text = df['price'][i]
+    availability = ET.SubElement(part, 'availability')
+    isAvailable = ET.SubElement(availability, 'isAvailable')
+    isAvailable.text = df['is_available'][i]
+    images = ET.SubElement(part, 'images')
+    image = ET.SubElement(images, 'image')
+    image.text = df['images'][i]
+    compatibility = ET.SubElement(part, 'compatibility')
+    compatibility.text = df['compatibility'][i]
+    tree = ET.ElementTree(parts)
+    
 
 
-# In[42]:
+# In[59]:
 
 
 tree = ET.ElementTree(parts)
-#mydata = ET.tostring(parts, encoding="utf-8", method="xml")
 tree.write("sample.xml", encoding='utf8')
-
-
-# In[63]:
-
-
-#def func(row):
-    #xml = ['<part>']
-    #for field in row.index:
-        #xml.append('  <{0}>{1}</field>'.format(field, row[field]))
-    #xml.append('</parts>')
-    #return '\n'.join(xml)
-
-
-# In[64]:
-
-
-#t = '\n'.join(df.apply(func, axis=1))
 
 
 # In[ ]:
