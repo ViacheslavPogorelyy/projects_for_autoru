9b262bd5880f95c92d0c608d517b748d31434d6e
ViacheslavPogorelyy <65274737+ViacheslavPogorelyy@users.noreply.github.com>
2021-08-05 23:47:57 +0300

    Add files via upload

--- license_plate/license_plate.py
+++ license_plate/license_plate.py
@@ -1,82 +1,120 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[55]:
+# In[378]:
 
 
-from PIL import Image
+from PIL import Image, ImageChops
 import requests
 from io import BytesIO
+import re 
+import imagehash
+import time
 
 
-# In[56]:
+# In[412]:
 
 
-offer = 'https://16.img.avito.st/640x480/11335020316.jpg'
+offer = 'http://05.img.avito.st/640x480/11336196705.jpg'
 
 
-# In[107]:
+# In[413]:
+
+
+offer_id = re.findall(r'\d{11}', offer)
+
+
+# In[414]:
+
+
+for element in offer_id:
+    offer_id = int(element)
+
+
+# In[415]:
+
+
+links = []
+
+for i in range(0, 300):
+    offer_id -= 1
+    last_num = str(offer_id)[-2:]
+    short_link = 'https://{}.img.avito.st/'.format(last_num)
+    all_link = short_link+'640x480/{}.jpg'.format(offer_id)
+    links.append(all_link)
+
+
+# In[416]:
 
 
 response = requests.get(offer)
 original = Image.open(BytesIO(response.content))
 
 
-# In[108]:
+# In[417]:
 
 
 original
 
 
-# In[87]:
+# In[418]:
 
 
-num = re.findall(r'\d{11}', offer)
+original_crop = original.crop((0, 0, 150, 150))
 
 
-# In[90]:
+# In[419]:
 
 
-for el in num:
-    el = int(el)
+original_crop
 
 
-# In[105]:
+# In[420]:
 
 
-links = []
-
-for i in range(0, 350):
-    num -= 1
-    lin ='https://01.img.avito.st/640x480/{}.jpg'.format(num)
-    links.append(lin)
+response2 = requests.get('https://05.img.avito.st/640x480/11335020005.jpg')
+test_image = Image.open(BytesIO(response2.content))
 
 
-# In[109]:
+# In[421]:
 
 
-response = requests.get(offer)
-original = Image.open(BytesIO(response.content))
+test_image_crop = test_image.crop((0, 0, 150, 150))
 
 
-# In[51]:
+# In[422]:
 
 
-response = requests.get('http://00.img.avito.st/640x480/11335020100.jpg')
-original2 = Image.open(BytesIO(response.content))
+test_image_crop
 
 
-# In[110]:
+# In[423]:
 
 
-result=ImageChops.difference(original, original2)
-result.show()
+hash_original = imagehash.average_hash(original_crop)
+hash_test = imagehash.average_hash(test_image_crop)
+print(hash_original - hash_test)
 
 
-# In[111]:
+# In[424]:
 
 
-result
+key = ()
+for i in links:
+    try:
+        response3 = requests.get(i)
+        test2 = Image.open(BytesIO(response3.content))
+        test_image_crop2 = test2.crop((0, 0, 150, 150))
+        hash_test2 = imagehash.average_hash(test_image_crop2)
+        different = hash_original - hash_test2
+        if different < 1:
+            print('Ссылка с номером:', i)
+            break
+        else:
+            е = 1
+    except:
+        pass
+    #time.sleep(1)
 
 
 # In[ ]:
